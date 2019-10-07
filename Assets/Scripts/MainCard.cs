b0ce97f07a24f9ec2e00394258ab74b1497b4e42
rojocado <48840215+rojocado@users.noreply.github.com>
2019-10-08 03:01:12 +0800

    Add files via upload
    starting point

---
+++ Assets/Scripts/MainCard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour
+{
+    [SerializeField] private SceneController controller;
+    [SerializeField] private GameObject Card_Back;
+
+    public void OnMouseDown()
+    {
+        if (Card_Back.activeSelf && controller.canReveal)
+        {
+            Card_Back.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    private int _id;
+    public int id
+    {
+        get {return _id;}
+    }
+
+    public void ChangeSprite(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image; //This gets the sprite renderer component an get the property of its sprite
+    }
+
+    public void Unreveal()
+    {
+        Card_Back.SetActive(true);
+    }
+}
